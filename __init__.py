5e6be70d68a008c8ad7097f44485109003543c9d
MHadiS <87732597+MHadiS@users.noreply.github.com>
2022-08-04 01:59:33 +0430

    Create __init__.py

---
+++ __init__.py
@@ -0,0 +1,12 @@
+from .pyGraph import Graph
+from .pyQueue import Queue
+from .pyStack import Stack
+from .pyLinkedList import LinkedList
+
+
+__all__ = (
+  Graph,
+  Queue,
+  Stack,
+  LinkedList,
+)
